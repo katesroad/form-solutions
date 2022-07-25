82d9cc7c66234b841d1e6a7f5c559abea3e99f79
Kate <kate@gmail.com>
2022-07-25 15:52:00 -0400

    Add folder alias for hook-form-demo

---
+++ apps/hook-form-demo/webpack.config.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const nrwlConfig = require('@nrwl/react/plugins/webpack.js')
+
+module.exports = (config, context) => {
+  nrwlConfig(config)
+
+  return merge(config, {
+    resolve: {
+      alias: {
+        '@assets': path.resolve(__dirname, './src/assets/'),
+        '@components': path.resolve(__dirname, './src/app/components')
+      },
+    },
+  });
+};
