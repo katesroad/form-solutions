fcc2011d866d01afe1e2ad91009266d6327ae812
Kate <kate@gmail.com>
2022-07-30 20:25:46 -0400

    Use ErrorMessage lib to consolidate error message to Select

--- apps/hook-form-demo/src/app/components/Dynamic/Dynamic.tsx
+++ apps/hook-form-demo/src/app/components/Dynamic/Dynamic.tsx
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
-import { ErrorMessage } from '@hookform/error-message';
 
-import FormField from './FormField';
 import Select from './Select';
 
 import { useGetClassTypes, useGetLimits } from './queries';
@@ -21,10 +19,10 @@ export default function Dynamic() {
 
   const classType = methods.watch('classType');
 
-  const { data: classTypes = [], isLoading: isLoadingClassTypes } =
+  const { data: classTypes, isLoading: isLoadingClassTypes } =
     useGetClassTypes();
 
-  const { data: limits = [], isLoading: isLoadingLimits } = useGetLimits(
+  const { data: limits, isLoading: isLoadingLimits } = useGetLimits(
     methods.getValues('classType')
   );
 
@@ -40,48 +38,37 @@ export default function Dynamic() {
     <FormProvider {...methods}>
       <form action="" onSubmit={handleSubmit}>
         <h3>Model information</h3>
-        <FormField label="class type">
-          <Select
-            name="classType"
-            disabled={isLoadingClassTypes}
-            required
-            options={[
-              {
-                label: isLoadingClassTypes
-                  ? `Is fetching class types...`
-                  : 'Please select class type',
-                value: '',
-              },
-              ...classTypes,
-            ]}
-          />
-          <ErrorMessage
-            errors={methods.formState.errors}
-            name="classType"
-            render={({ message }) => <p className="error">{message}</p>}
-          />
-        </FormField>
-        <FormField label="limit">
-          <Select
-            name="limit"
-            disabled={!classType}
-            required
-            options={[
-              {
-                label: (!!classType && isLoadingLimits)
-                    ? `Is fetching limit...`
-                    : 'Please select limit',
-                value: '',
-              },
-              ...limits,
-            ]}
-          />
-          <ErrorMessage
-            errors={methods.formState.errors}
-            name="limit"
-            render={({ message }) => <p className="error">{message}</p>}
-          />
-        </FormField>
+        <Select
+          label="class type"
+          name="classType"
+          disabled={isLoadingClassTypes}
+          validation={{ required: 'class type is required' }}
+          options={[
+            {
+              label: isLoadingClassTypes
+                ? `Is fetching class types...`
+                : 'Please select class type',
+              value: '',
+            },
+            ...(classTypes ?? []),
+          ]}
+        />
+        <Select
+          label="limit"
+          name="limit"
+          disabled={!classType}
+          validation={{ required: 'limit is required' }}
+          options={[
+            {
+              label:
+                !!classType && isLoadingLimits
+                  ? `Is fetching limit...`
+                  : 'Please select limit',
+              value: '',
+            },
+            ...(limits ?? []),
+          ]}
+        />
         <button
           type="submit"
           disabled={!methods.formState.isValid && methods.formState.isDirty}
--- apps/hook-form-demo/src/app/components/Dynamic/Select.tsx
+++ apps/hook-form-demo/src/app/components/Dynamic/Select.tsx
@@ -1,30 +1,55 @@
 import { useFormContext } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+
+import FormField from './FormField';
 
 export type Props = {
   options: { label: string; value: string }[];
   name: string;
   disabled?: boolean;
-  required?: boolean;
+  label: string;
+  validation?: { required: boolean | string };
 };
 
-export default function Select({ name, options, disabled, required }: Props) {
-  const { register, getValues } = useFormContext();
+export default function Select({
+  name,
+  options,
+  disabled,
+  validation = { required: false },
+  label,
+}: Props) {
+  const {
+    register,
+    getValues,
+    formState: { errors },
+  } = useFormContext();
 
   const value = getValues(name);
 
   return (
-    <select
-      {...register(name, {
-        disabled,
-        required: required ? `${name} is required` : undefined,
-      })}
-      defaultValue={value}
-    >
-      {options.map((option) => (
-        <option value={option.value} key={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
+    <FormField label={label}>
+      <select
+        {...register(name, {
+          disabled,
+          ...validation,
+        })}
+        defaultValue={value}
+      >
+        {options.map((option) => (
+          <option value={option.value} key={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <ErrorMessage
+        errors={errors}
+        name={name}
+        render={({ message }) => (
+          <p className="error">
+            {message ?? `${label.toLowerCase()} is required`}
+          </p>
+        )}
+      />
+    </FormField>
   );
 }
